4d18e4b5a9738c5b5c718f889b9356fd5348e0fe
Frank Skare <frank.skare.de@gmail.com>
2020-04-11 12:24:31 +0200

    6.0.0.0

--- Release.ps1
+++ Release.ps1
@@ -2,5 +2,5 @@
 $exePath = '.\src\bin\MediaInfoNET.exe'
 $version = [Diagnostics.FileVersionInfo]::GetVersionInfo($exePath).FileVersion
 $targetDir = [Environment]::GetFolderPath('Desktop') + '\MediaInfo.NET-' + $version
-Copy-Item .\src\bin $targetDir -Recurse -Exclude *.runtimeconfig.dev.json
+Copy-Item .\src\bin $targetDir
 & 'C:\Program Files\7-Zip\7z.exe' a -t7z -mx9 "$targetDir.7z" -r "$targetDir\*"
