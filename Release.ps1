6b7fca63e50b7e02c770f047398c9a8644aa81af
Frank Skare <frank.skare.de@gmail.com>
2021-03-16 01:37:33 +0100

    release script

--- Release.ps1
+++ Release.ps1
@@ -1,6 +1,6 @@
 
 $exePath = '.\src\bin\MediaInfoNET.exe'
 $version = [Diagnostics.FileVersionInfo]::GetVersionInfo($exePath).FileVersion
-$targetDir = [Environment]::GetFolderPath('Desktop') + '\MediaInfo.NET-' + $version
+$targetDir = 'D:\Work\MediaInfo.NET-' + $version
 Copy-Item .\src\bin $targetDir -Recurse -Exclude System.Management.Automation.xml
 & 'C:\Program Files\7-Zip\7z.exe' a -tzip -mx9 "$targetDir.zip" -r "$targetDir\*"
